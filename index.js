1355feb245bbedbc64d325c393805cda6aa7b724
ParithiDB <samu.aidindia@gmail.com>
2023-09-25 21:48:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+app.use(cors());
+
+const server = http.createServer(app);
+
+const PORT = 5000;
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data.room);
+    console.log(`User with ID: ${socket.id} joined room: ${data.room}`);
+  
+    // Broadcast the join message to all clients in the room
+    const joinMessage = `${data.username} joined the chat`;
+    io.to(data.room).emit("receive_message", { message: joinMessage, author: "System" });
+  });
+  
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log("SERVER RUNNING ON PORT", PORT);
+});

1def180f68202d3c22457e07ed013efcf1748fc0
ParithiDB <samu.aidindia@gmail.com>
2023-09-25 21:59:16 +0530

    netlify link

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT = 5000;
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://jocular-boba-d481bb.netlify.app/",
     methods: ["GET", "POST"],
   },
 });
